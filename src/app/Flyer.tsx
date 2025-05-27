204d382c62af7750fc81df2786f16aa95d8e796a
Duvan Vargas <duvan.vargas@soytul.com>
2025-05-26 23:11:14 -0500

    black cover

--- src/app/Flyer.tsx
+++ src/app/Flyer.tsx
@@ -37,10 +37,10 @@ const Flyer = () => {
   }
 
   return (
-    <div className="animate-fade-in">
-      <div className="absolute top-0 left-0 p-5 font-medium">APRETAITO</div>
-      <div className="absolute top-0 right-0 p-5">0x00000002</div>
-      <div className="absolute top-20 left-0 p-5">
+    <div className="animate-fade-in ">
+      <div className="absolute top-0 left-0 p-5 font-medium ">APRETAITO</div>
+      <div className="absolute top-0 right-0 p-5 ">0x00000002</div>
+      <div className="absolute top-20 left-0 p-5 ">
         {djs.map((dj, index) => (
           <div
             key={index}
@@ -51,15 +51,23 @@ const Flyer = () => {
           </div>
         ))}
       </div>
-      <div className="absolute bottom-0 right-0 p-5">
-        <RandomReveal
-          isPlaying
-          duration={3}
-          characters={"Calle 56 # 3-34, APTO 208"}
-        />
-      </div>
-      <div className="absolute bottom-10 right-0 p-5">
-        <RandomReveal isPlaying duration={3} characters={"2 JUN, 5AM - ???"} />
+      <div className="absolute bottom-5 right-0 p-5  rounded ">
+        <div className="fixed bottom-0 left-0 w-full bg-black h-[90px] p-5">
+          <div className="text-right">
+            <RandomReveal
+              isPlaying
+              duration={3}
+              characters={"Calle 56 # 3-34, APTO 208"}
+            />
+          </div>
+          <div className="text-right">
+            <RandomReveal
+              isPlaying
+              duration={3}
+              characters={"2 JUN, 5AM - ???"}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Flyer from "./Flyer";
 
 export default function Home() {
   return (
-    <main className="pointer-events-none">
+    <main className="h-full bg-black  flex-col items-center justify-center text-white">
       <Flyer />
-      <Spline scene="https://prod.spline.design/qJ-xqtbUNNTgxbVG/scene.splinecode" />
+      <div className="">
+        <Spline scene="https://prod.spline.design/qJ-xqtbUNNTgxbVG/scene.splinecode" />
+      </div>
     </main>
   );
 }
